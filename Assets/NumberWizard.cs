a4788e092b8b2f32908630ad9e5765ac38ea824c
poudigne <pierluc0414@live.ca>
2015-02-24 20:39:39 -0500

    Number wizard

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	public int min = 1;
+	public int max = 1000;
+	
+	public Text guessText;
+	
+	int currentlyGuessing;
+	void Start(){
+		StartGame();
+	}
+	void StartGame(){
+		NextGuessing();
+		max += 1;
+	}
+	
+	public void GuessHigher(){
+		this.min = this.currentlyGuessing;
+		NextGuessing();
+	}
+	
+	public void GuessLower(){
+		this.max = this.currentlyGuessing;
+		NextGuessing();
+	}
+	
+	void NextGuessing(){
+		currentlyGuessing = Random.Range (min,max);
+		guessText.text = currentlyGuessing.ToString();
+	}
+	
+}
